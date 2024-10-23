7692b8841279cc561d037f00766de1db4a840223
Mateus <mateusssilva85@gmail.com>
2024-10-23 12:37:20 -0300

    Creating and styling password requ

--- page/RedefinePassword/script.js
+++ page/RedefinePassword/script.js
@@ -1,5 +1,12 @@
 const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.login__button');
+const button = document.querySelector('.button__password');
+
+const minlength = document.getElementById('min__length');
+const uppercase = document.getElementById('uppercase');
+const number = document.getElementById('number');
+const specialChar = document.getElementById('special__char');
+
+
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -14,13 +21,42 @@ const handleFocusOut = ({ target }) => {
 }
 
 const handleChange = () => {
-    const [username, password] = inputs;
+    const [email, newPassword, confirmPassword] = inputs;
 
-    if (username.value && password.value.length >= 8) {
+    const emailIsValid = email.value.includes('@') && email.value.includes('.');
+
+    const minLengthIsValid = newPassword.value.length >= 8;
+    const containsUppercase = /[A-Z]/.test(newPassword.value);
+    const containsNumber = /\d/.test(newPassword.value);
+    const containsSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(newPassword.value);
+
+    minlength.classList.toggle('valid', minLengthIsValid);
+    minlength.classList.toggle('invalid', !minLengthIsValid);
+
+    uppercase.classList.toggle('valid', containsUppercase);
+    uppercase.classList.toggle('invalid', !containsUppercase);
+
+number.classList.toggle('valid' , containsNumber);
+number.classList.toggle('invalid' , !containsNumber);
+
+specialChar.classList.toggle('valid' , containsSpecialChar);
+specialChar.classList.toggle('invalid' , !containsSpecialChar);
+
+
+
+
+
+
+    const passwordsAreValid = newPassword.value.length >= 8 && confirmPassword.value.length >= 8 && newPassword.value === confirmPassword.value;
+
+    if (emailIsValid && passwordsAreValid) {
         button.removeAttribute('disabled');
-    }
+        button.style.backgroundColor = 'var(--red)';
+    } 
+    
     else {
         button.setAttribute('disabled', '');
+        button.style.backgroundColor = 'var(--medium-grey)';
     }
 }
 
