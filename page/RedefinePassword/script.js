f0d5fe483840a7056540b39c64d53ae828294961
Mateus <mateusssilva85@gmail.com>
2024-10-23 14:13:06 -0300

    Adding the “view password” icon and giving it a function.

--- page/RedefinePassword/script.js
+++ page/RedefinePassword/script.js
@@ -1,6 +1,12 @@
 const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.button__password');
 
+const togglePassword = document.getElementById('togglePassword');
+const newPassword = document.getElementById('newPassword');
+
+const toggleConfirmPassword = document.getElementById ('toggleConfirmPassword');
+
+
 const minlength = document.getElementById('min__length');
 const uppercase = document.getElementById('uppercase');
 const number = document.getElementById('number');
@@ -43,10 +49,6 @@ specialChar.classList.toggle('valid' , containsSpecialChar);
 specialChar.classList.toggle('invalid' , !containsSpecialChar);
 
 
-
-
-
-
     const passwordsAreValid = newPassword.value.length >= 8 && confirmPassword.value.length >= 8 && newPassword.value === confirmPassword.value;
 
     if (emailIsValid && passwordsAreValid) {
@@ -59,6 +61,23 @@ specialChar.classList.toggle('invalid' , !containsSpecialChar);
         button.style.backgroundColor = 'var(--medium-grey)';
     }
 }
+
+function togglePasswordVisibility(toggleId, inputId) {
+    const toggleIcon = document.getElementById(toggleId);
+    const passwordInput = document.getElementById(inputId);
+
+    toggleIcon.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        
+        this.classList.toggle('bi-eye-fill');
+        this.classList.toggle('bi-eye-slash');
+    });
+}
+
+togglePasswordVisibility('togglePassword', 'newPassword');
+togglePasswordVisibility('toggleConfirmPassword', 'confirmPassword');
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
