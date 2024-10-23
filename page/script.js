c5780fb3e4aa73a7eb76e7cf9b376e7387f0ae39
Mateus <mateusssilva85@gmail.com>
2024-10-23 19:04:52 -0300

    worAdd image to “Reset passd” folder and add view password icon to “login page” folder.

--- page/script.js
+++ page/script.js
@@ -13,6 +13,25 @@ const handleFocusOut = ({ target }) => {
     }
 }
 
+
+function togglePasswordVisibility(toggleId, inputId) {
+    const toggleIcon = document.getElementById(toggleId);  
+    const passwordInput = document.getElementById(inputId); 
+
+    toggleIcon.addEventListener('click', function () {
+        
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+    
+        this.classList.toggle('bi-eye-fill');
+        this.classList.toggle('bi-eye-slash');
+    });
+}
+
+togglePasswordVisibility('togglePassword', 'password');
+
+
 function openInNewTab(buttonId, url) {
     document.getElementById(buttonId).addEventListener('click', function () {
         window.open(url, '_blank');
